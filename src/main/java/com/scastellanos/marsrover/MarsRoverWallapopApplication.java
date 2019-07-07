e257f7ae49c8a3312bcb63ff7539f19ddd180e07
scastellanos <sebastianxcf@gmail.com>
2019-07-07 18:27:57 +0200

    hello mars-rover spring boot

---
+++ src/main/java/com/scastellanos/marsrover/MarsRoverWallapopApplication.java
@@ -0,0 +1,13 @@
+package com.scastellanos.marsrover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsRoverWallapopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsRoverWallapopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scastellanos/marsrover/MarsRoverWallapopApplicationTests.java
@@ -0,0 +1,16 @@
+package com.scastellanos.marsrover;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MarsRoverWallapopApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
